53850012d7016a4d8c61e4365ec1e6ecd4f05245
Bruno Brito <bruno.debrito@tudelft.nl>
2019-08-26 10:06:28 +0200

    added noise to goal

--- pedsim_simulator/include/pedsim_simulator/element/waypoint.h
+++ pedsim_simulator/include/pedsim_simulator/element/waypoint.h
@@ -56,6 +56,8 @@ class Waypoint : public ScenarioElement, public Ped::Twaypoint {
   virtual void setPosition(const Ped::Tvector& posIn);
   virtual void setx(double xIn);
   virtual void sety(double yIn);
+  //virtual double getx();
+  //virtual double gety();
   virtual void setRadius(double rIn);
 
   // Attributes
